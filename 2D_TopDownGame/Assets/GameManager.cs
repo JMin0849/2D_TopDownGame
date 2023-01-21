892bf13a6d5715b1c250b3aed4c1cc37fac6e3f5
JMin0849 <5959kop@naver.com>
2023-01-21 15:58:01 +0900

    2D_TopDownGame
    골드메탈 B25 대화 애니메이션 만들기

--- 2D_TopDownGame/Assets/GameManager.cs
+++ 2D_TopDownGame/Assets/GameManager.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public Image portraitImg;
+    public Animator portraitAnim;
+    public Sprite prePortrait;
     public QuestManager questManager;
     public TalkManager talkManager;
-    public GameObject talkPanel;
-    public Text talkText;
+    public Animator talkPanel;
+    public TypeEffect talk;
     public GameObject scanObject;
     public bool isAction;
     public int talkIndex;
@@ -18,16 +20,29 @@ public class GameManager : MonoBehaviour
         Debug.Log(questManager.CheckQuest());
     }
 public void Action(GameObject scanObj){
-    scanObject = scanObj;
-    ObjData objData = scanObject.GetComponent<ObjData>();
-    Talk(objData.id, objData.isNpc);
+        //Get Current Object
+        scanObject = scanObj;
+        ObjData objData = scanObject.GetComponent<ObjData>();
+        Talk(objData.id, objData.isNpc);
     
-    talkPanel.SetActive(isAction);
+        //Visible Talk for Action
+        talkPanel.SetBool("isShow", isAction);
 }
 void Talk(int id, bool isNpc){
-    //Set Talk Data
-    int questTalkIndex = questManager.GetQuestTalkIndex(id);
-    string talkData = talkManager.GetTalk(id + questTalkIndex, talkIndex);
+        int questTalkIndex = 0;
+        string talkData = "";
+
+        //Set Talk Data
+        if (talk.isAnim)
+        {
+            talk.SetMsg("");
+            return;
+        }
+        else
+        {
+            questTalkIndex = questManager.GetQuestTalkIndex(id);
+            talkData = talkManager.GetTalk(id + questTalkIndex, talkIndex);
+        }
 
     //End Talk
     if(talkData == null){
@@ -39,14 +54,22 @@ void Talk(int id, bool isNpc){
 
     //Continue Talk
     if(isNpc){
-        talkText.text = talkData.Split(':')[0];
-
+        talk.SetMsg(talkData.Split(':')[0]);
+        
+        //Show portrait
         portraitImg.sprite = talkManager.GetPortrait(id,  int.Parse(talkData.Split(':')[1]));
         portraitImg.color = new Color(1,1,1,1);
+
+        //Animation portrait
+        if (prePortrait != portraitImg.sprite)
+            {
+                portraitAnim.SetTrigger("doEffect");
+                prePortrait = portraitImg.sprite;
+            }
         
     }
     else{
-        talkText.text = talkData;
+        talk.SetMsg(talkData);
 
         portraitImg.color = new Color(1,1,1,0);
 
